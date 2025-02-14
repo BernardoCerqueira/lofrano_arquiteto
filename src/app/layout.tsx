412a076651ec38ed86e7a914a7147db5647524a5
Bernardo Cerqueira <katanardo@gmail.com>
2025-02-14 17:30:03 -0300

    Favicon and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,21 +11,22 @@ const lato = Lato({
 
 export const metadata: Metadata = {
   title: "Lofrano Arquitetura",
-  description: "Escritório de Arquitetura - Transforme seus sonhos em realidade com projetos sob medida feitos por especialistas.",
+  description: "Escritório de Arquitetura - Transforme seus sonhos em realidade com projetos sob medida feitos por especialistas - Localizado em Petrópolis/RJ e com atuação remota.",
+  
   openGraph: {
     type: "website",
     description: "Escritório de Arquitetura - Transforme seus sonhos em realidade com projetos sob medida feitos por especialistas.",
     title: "Lofrano Arquitetura",
     images: [
       {
-        url: "https://lofranoarquitetura.com.br/logo.png",
+        url: "https://lofranoarquitetura.com.br/favicon.ico",
         width: 600,
         height: 600,
         alt: "Lofrano Arquitetura"
       }
     ]
   },
-  keywords: "arquitetura, lofrano, arquiteto, petrópolis, rio de janeiro, design, interiores, construção, projetos, projeto, arquitetônico, joão, pedro"
+  keywords: "arquitetura, lofrano, arquiteto, petrópolis, rio de janeiro, design, interiores, construção, projetos, projeto, arquitetônico, joão, pedro, serra, serrana, arquiteta, joão pedro"
 };
 
 export default function RootLayout({
