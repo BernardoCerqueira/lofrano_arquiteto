a8be345732e6dda745d579afcf7e0523ab225892
Bernardo Cerqueira <katanardo@gmail.com>
2025-02-17 12:44:14 -0300

    Slides da página de projeto

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,11 +1,73 @@
-import styles from "./Projects.module.scss"
+"use client"
 
-export default function Projects(){
-    return(
+import styles from "./Projects.module.scss"
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/css";
+import Image from "next/image";
+
+export default function Projects() {
+    return (
         <div className={styles.container}>
             <h2 className={styles.title}>Projetos</h2>
             <div className={styles.slideDiv}>
-
+                <Splide
+                    options={{
+                        type: "loop",
+                        perPage: 2,
+                        gap: "2vw",
+                        autoplay: true,
+                        interval: 6500,
+                        arrows: true,
+                        pagination: true,
+                        breakpoints: {
+                            1100:
+                            {
+                                perPage: 1
+                            }
+                        },
+                    }}
+                    className={styles.splide}
+                >
+                    <SplideSlide className={styles.slide}>
+                        <div>
+                            <Image
+                                src="/studio-chilli1.png"
+                                alt="Projeto 1"
+                                height={400}
+                                width={700}
+                            />
+                            <p className={styles.caption}>
+                                Estúdio Banda Chilli 1
+                            </p>
+                        </div>
+                    </SplideSlide>
+                    <SplideSlide className={styles.slide}>
+                        <div>
+                            <Image
+                                src="/studio-chilli1.png"
+                                alt="Projeto 1"
+                                height={400}
+                                width={700}
+                            />
+                            <p className={styles.caption}>
+                                Estúdio Banda Chilli 2
+                            </p>
+                        </div>
+                    </SplideSlide>
+                    <SplideSlide className={styles.slide}>
+                        <div>
+                            <Image
+                                src="/banheiro-reformado.png"
+                                alt="Projeto 1"
+                                height={400}
+                                width={700}
+                            />
+                            <p className={styles.caption}>
+                                Reforma Banheiro
+                            </p>
+                        </div>
+                    </SplideSlide>
+                </Splide>
             </div>
         </div>
     )
