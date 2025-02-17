a3dbec38a2228ff47ddde6f6709eace6566ce060
Bernardo Cerqueira <katanardo@gmail.com>
2025-02-17 15:33:56 -0300

    For Architects Section

--- src/components/ForArchitects/index.tsx
+++ src/components/ForArchitects/index.tsx
@@ -1,8 +1,42 @@
-import style from "./ForArchitects.module.scss"
+"use client"
 
-export default function ForArchitects(){
-    return(
-        <>
-        </>
+import styles from "./ForArchitects.module.scss"
+import { FaFacebookSquare, FaInstagram, FaWhatsapp } from "react-icons/fa"
+import Link from "next/link"
+
+export default function ForArchitects() {
+    return (
+        <div className={styles.container}>
+            <h2 className={styles.title}>Para Arquitetos</h2>
+            <div className={styles.contentDiv}>
+                <div className={styles.subtitle}>
+                    <span>
+                        Maquete 3D <span className={styles.bullet}>•</span> Plantas Baixas <span className={styles.bullet}>•
+                        </span>
+                    </span>
+                    <span>
+                        Projetos Executivos <span className={styles.bullet}>•</span> Projeto Legal
+                    </span>
+                </div>
+                <p className={styles.text}>Entre em contato para contratar nossos serviços de freelancer. Somos especializados na elaboração de maquetes eletrônicas, desenvolvimento de plantas baixas e oferecemos suporte em projetos executivos, além de assessorarmos na regularização de projetos junto aos órgãos competentes.</p>
+                <div className={styles.btnDiv}>
+                    <Link href={"https://www.facebook.com"}>
+                        <button className={styles.btn}>
+                            <FaFacebookSquare className={styles.icon} />
+                        </button>
+                    </Link>
+                    <Link href={"https://www.facebook.com"}>
+                        <button className={styles.btn}>
+                            <FaInstagram className={styles.icon} />
+                        </button>
+                    </Link>
+                    <Link href={"https://www.facebook.com"}>
+                        <button className={styles.btn}>
+                            <FaWhatsapp className={styles.icon} />
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
     )
 }
