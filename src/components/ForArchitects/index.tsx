ee9dcfa641f90e6175b5acebfa9cb9638d413ddb
Bernardo Cerqueira <katanardo@gmail.com>
2025-02-17 16:25:25 -0300

    Responsividade da página "Para Arquitetos"

--- src/components/ForArchitects/index.tsx
+++ src/components/ForArchitects/index.tsx
@@ -10,27 +10,47 @@ export default function ForArchitects() {
             <h2 className={styles.title}>Para Arquitetos</h2>
             <div className={styles.contentDiv}>
                 <div className={styles.subtitle}>
-                    <span>
+                    <p>
                         Maquete 3D <span className={styles.bullet}>•</span> Plantas Baixas <span className={styles.bullet}>•
                         </span>
-                    </span>
-                    <span>
+                    </p>
+                    <p>
                         Projetos Executivos <span className={styles.bullet}>•</span> Projeto Legal
-                    </span>
+                    </p>
+                </div>
+                <div className={styles.listDiv}>
+                    <ul className={styles.list}>
+                        <li><p>Maquete 3D</p></li>
+                        <li><p>Plantas Baixas</p></li>
+                        <li><p>Projetos Executivos</p></li>
+                        <li><p>Projeto Legal</p></li>
+                    </ul>
                 </div>
                 <p className={styles.text}>Entre em contato para contratar nossos serviços de freelancer. Somos especializados na elaboração de maquetes eletrônicas, desenvolvimento de plantas baixas e oferecemos suporte em projetos executivos, além de assessorarmos na regularização de projetos junto aos órgãos competentes.</p>
                 <div className={styles.btnDiv}>
-                    <Link href={"https://www.facebook.com"}>
+                    <Link
+                        href={"https://www.facebook.com/people/Lofrano-Arquitetura/61572186776421/?_rdr"}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
                         <button className={styles.btn}>
                             <FaFacebookSquare className={styles.icon} />
                         </button>
                     </Link>
-                    <Link href={"https://www.facebook.com"}>
+                    <Link
+                        href={"https://www.instagram.com/lofrano.arq/"}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
                         <button className={styles.btn}>
                             <FaInstagram className={styles.icon} />
                         </button>
                     </Link>
-                    <Link href={"https://www.facebook.com"}>
+                    <Link
+                        href={"https://wa.me/5524988139765"}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
                         <button className={styles.btn}>
                             <FaWhatsapp className={styles.icon} />
                         </button>
