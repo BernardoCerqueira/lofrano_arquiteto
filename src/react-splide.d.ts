1b42ad6979db1b6a5ddb3e68fd62a96ef5b62f65
Bernardo Cerqueira <katanardo@gmail.com>
2025-02-17 13:03:59 -0300

    Tipagem do Splide

--- src/react-splide.d.ts
+++ src/react-splide.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module "@splidejs/react-splide" {
   import { ComponentType } from "react";
   export const Splide: ComponentType<any>;
