dd1883963e9f67b154a01cd484c1f5946dc22099
Bernardo Cerqueira <katanardo@gmail.com>
2025-02-17 17:21:05 -0300

    Form Section

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,6 +1,76 @@
-export default function Contact(){
-    return(
-        <>
-        </>
+"use client"
+
+import { Button, Form, Input } from "reactstrap"
+import styles from "./Contact.module.scss"
+import Link from "next/link"
+import { FaFacebookSquare, FaInstagram, FaWhatsapp } from "react-icons/fa"
+
+export default function Contact() {
+    return (
+        <div className={styles.container}>
+            <h2 className={styles.title}>Contatos</h2>
+            <div className={styles.contentDiv}>
+                <Form action="" className={styles.form}>
+                    <legend className={styles.formTitle}>Orçamentos:</legend>
+                    <div className={styles.inputDiv}>
+                        <Input
+                            type="text"
+                            className={styles.input}
+                            placeholder="Nome:"
+                        />
+                        <Input
+                            type="tel"
+                            className={styles.input}
+                            placeholder="Celular:"
+                        />
+                        <Input
+                            type="email"
+                            className={styles.input}
+                            placeholder="E-mail:"
+                        />
+                        <Input
+                            type="text"
+                            className={styles.input}
+                            placeholder="Cidade:"
+                        />
+                    </div>
+                    <Button className={styles.formBtn}>
+                        Enviar
+                    </Button>
+                </Form>
+                <div className={styles.contactDiv}>
+                    <p className={styles.andOr}>E/OU</p>
+                    <div className={styles.btnDiv}>
+                        <Link
+                            href={"https://www.facebook.com/people/Lofrano-Arquitetura/61572186776421/?_rdr"}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <button className={styles.btn}>
+                                <FaFacebookSquare className={styles.icon} />
+                            </button>
+                        </Link>
+                        <Link
+                            href={"https://www.instagram.com/lofrano.arq/"}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <button className={styles.btn}>
+                                <FaInstagram className={styles.icon} />
+                            </button>
+                        </Link>
+                        <Link
+                            href={"https://wa.me/5524988139765"}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <button className={styles.btn}>
+                                <FaWhatsapp className={styles.icon} />
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
