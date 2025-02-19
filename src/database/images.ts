bdc2962058774346010ee53d762d4dcfcbdad388
Bernardo Cerqueira <katanardo@gmail.com>
2025-02-19 12:22:48 -0300

    Novos projetos

--- src/database/images.ts
+++ src/database/images.ts
@@ -25,6 +25,41 @@ const database: Database = [
         imgPath: "/projects/banheiro01.png",
         alt: "Projeto de Banheiro"
     },
+    {
+        id: 5,
+        imgPath: "/projects/cozinha01.PNG",
+        alt: "Projeto de Cozinha 1"
+    },
+    {
+        id: 6,
+        imgPath: "/projects/quarto01.PNG",
+        alt: "Projeto de Quarto 1"
+    },
+    {
+        id: 7,
+        imgPath: "/projects/quarto02.PNG",
+        alt: "Projeto de Quarto 2"
+    },
+    {
+        id: 8,
+        imgPath: "/projects/quartoModerno01.JPG",
+        alt: "Projeto de Quarto Moderno"
+    },
+    {
+        id: 9,
+        imgPath: "/projects/sala01.PNG",
+        alt: "Projeto de Sala 1"
+    },
+    {
+        id: 10,
+        imgPath: "/projects/sala02.PNG",
+        alt: "Projeto de Sala 2"
+    },
+    {
+        id: 11,
+        imgPath: "/projects/salaModerna01.png",
+        alt: "Projeto de Sala Moderna"
+    },
 ]
 
 export default database
