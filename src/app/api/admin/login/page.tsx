d6b69afbc18142336ae2763b9c719cb9d350ff8f
Bernardo Cerqueira <katanardo@gmail.com>
2025-02-19 19:46:20 -0300

    Estilização Login Admin

--- src/app/api/admin/login/page.tsx
+++ src/app/api/admin/login/page.tsx
@@ -1,11 +1,46 @@
+import styles from "./Login.module.scss"
+
 export default function Login() {
     return (
-        <>
-            <form action="/api/admin">
-                <input type="text" placeholder="email" name="email" />
-                <input type="text" placeholder="senha" name="password" />
-                <button type="submit">Enviar</button>
+        <div className={styles.container}>
+            <h1 className={styles.title}>
+                Login de Administrador
+            </h1>
+            <form action="/api/admin" className={styles.form}>
+                <div>
+                    <label
+                        htmlFor="email"
+                        className={styles.label}
+                    >
+                        E-mail:
+                    </label>
+                    <input
+                        type="email"
+                        placeholder="Digite seu email..."
+                        name="email"
+                        id="email"
+                        className={styles.input} 
+                    />
+                </div>
+                <div>
+                    <label
+                        htmlFor="password"
+                        className={styles.label}
+                    >
+                        Senha:
+                    </label>
+                    <input
+                        type="password"
+                        placeholder="Digite sua senha.."
+                        name="password"
+                        id="password"
+                        className={styles.input}
+                    />
+                </div>
+                <button type="submit" className={styles.button}>
+                    Entrar
+                </button>
             </form>
-        </>
+        </div>
     )
 }
